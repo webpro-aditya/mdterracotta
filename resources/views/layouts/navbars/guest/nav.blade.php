ae3ef1f2c0458215c2f6a6d0ec8d366e0a6caa20
Aditya D <webpro.aditya@gmail.com>
2025-08-02 15:48:21 +0530

    mdy style

--- resources/views/layouts/navbars/guest/nav.blade.php
+++ resources/views/layouts/navbars/guest/nav.blade.php
@@ -12,13 +12,13 @@
     
     <!-- Navigation Menu (responsive) -->
     <ul class="nav-menu hidden md:flex md:flex-row flex-wrap justify-center space-y-4 md:space-y-0 md:space-x-8 text-lg font-medium w-full md:w-auto" id="main-nav-menu">
-      <li><a href="{{ route('home') }}" class="hover:text-teal-400 transition duration-300 nav-link text-teal-300 block py-2 px-4 rounded-lg">Home</a></li>
-      <li><a href="{{ route('front.about') }}" class="hover:text-teal-400 transition duration-300 nav-link block py-2 px-4 rounded-lg">About Us</a></li>
-      <li><a href="{{ route('front.management') }}" class="hover:text-teal-400 transition duration-300 nav-link block py-2 px-4 rounded-lg">Management</a></li>
-      <li><a href="{{ route('front.ourStory') }}" class="hover:text-teal-400 transition duration-300 nav-link block py-2 px-4 rounded-lg">Our Story</a></li>
-      <li><a href="{{ route('front.products') }}" class="hover:text-teal-400 transition duration-300 nav-link block py-2 px-4 rounded-lg">Products</a></li>
-      <li><a href="{{ route('front.events') }}" class="hover:text-teal-400 transition duration-300 nav-link block py-2 px-4 rounded-lg">Events</a></li>
-      <li><a href="{{ route('front.contact') }}" class="hover:text-teal-400 transition duration-300 nav-link block py-2 px-4 rounded-lg">Contact Us</a></li>
+      <li><a href="{{ route('home') }}" class="hover:text-teal-400 transition duration-300 nav-link block py-2 px-4 rounded-lg @if(Request::routeIs('home')) text-teal-300 @endif">Home</a></li>
+      <li><a href="{{ route('front.about') }}" class="hover:text-teal-400 transition duration-300 nav-link block py-2 px-4 rounded-lg @if(Request::routeIs('front.about')) text-teal-300 @endif">About Us</a></li>
+      <li><a href="{{ route('front.management') }}" class="hover:text-teal-400 transition duration-300 nav-link block py-2 px-4 rounded-lg @if(Request::routeIs('front.management')) text-teal-300 @endif">Management</a></li>
+      <li><a href="{{ route('front.ourStory') }}" class="hover:text-teal-400 transition duration-300 nav-link block py-2 px-4 rounded-lg @if(Request::routeIs('front.ourStory')) text-teal-300 @endif">Our Story</a></li>
+      <li><a href="{{ route('front.products') }}" class="hover:text-teal-400 transition duration-300 nav-link block py-2 px-4 rounded-lg @if(Request::routeIs('front.products')) text-teal-300 @endif">Products</a></li>
+      <li><a href="{{ route('front.events') }}" class="hover:text-teal-400 transition duration-300 nav-link block py-2 px-4 rounded-lg @if(Request::routeIs('front.events')) text-teal-300 @endif">Events</a></li>
+      <li><a href="{{ route('front.contact') }}" class="hover:text-teal-400 transition duration-300 nav-link block py-2 px-4 rounded-lg @if(Request::routeIs('front.contact')) text-teal-300 @endif">Contact Us</a></li>
     </ul>
   </nav>
 </header>
